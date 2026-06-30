bf87746ab367c78157783cd0e9d42e4cd107a2e7
duckyduck01 <madhvetipathak2006@gmail.com>
2026-06-30 13:50:09 +0530

    added permissions and registered models on the admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import *
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Department)
+admin.site.register(QuestionSet)
+admin.site.register(Exam)
+admin.site.register(Candidate)
+admin.site.register(Teacher)
